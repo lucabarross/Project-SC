1b466babe98fb18589dea096e2ee57f6c63828ce
Tupacao <arthur12268@gmail.com>
2024-08-15 00:53:57 -0300

    Added colors

---
+++ src/Config/Colors.tsx
@@ -0,0 +1,9 @@
+export const colors = {
+    acento: "#481D73",
+    acento_lighty: "#7D37C1",
+    primary: "#381659",
+    secondary: "#281040",
+    dark: "#150926",
+    darker: "#06020D",
+    background: "#090808" 
+};
